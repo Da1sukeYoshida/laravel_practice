1c9e0e2679f7d676bb201a3bb95b3ed33d4736d3
Daisei Yoshino <sen.daiseiyoshino@gmail.com>
2019-03-08 12:07:49 +0900

    auth login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,14 @@
 	<meta charset='utf-8'>
 </head>
 <body>
-	こんにちは！<br>
+	こんにちは！
+	@if (Auth::check())
+		{{ \Auth::user()->name }}さん<br />
+		<a href="/auth/logout">ログアウト</a>
+	@else
+		ゲストさん<br />
+		<a href="/auth/login">ログイン<br />
+		<a href="/auth/register">会員登録</a>
+	@endif
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -70,3 +70,11 @@ Route::post('/task/{task}', function(Task $task, Request $request){
 
         return redirect('/');
 });
+
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register', 'Auth\RegisterController@register');
+
+Route::get('/auth/login', 'Auth\LoginController@showLoginForm');
+Route::post('/auth/login', 'Auth\LoginController@login');
+
+Route::get('/auth/logout', 'Auth\LoginController@logout');
